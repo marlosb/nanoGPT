cc1ea44dbc92cff5b6ee217e7cb050e2c2d66dac
Marlos Bosso <36383314+marlosb@users.noreply.github.com>
2025-12-11 07:03:26 -0300

    update train config file for my tokenizer

--- config/train_mytokenizer.py
+++ config/train_mytokenizer.py
@@ -1,9 +1,8 @@
-# config for training mymodel (81M) down to very nice loss of ??? 
-# Ran 850,000 iterations on 1 node with 2x A100 40GB
-# Ran more 1,550,000 iterations on 1 node with 1x H100 95GB
-# For a total of 580B tokens (~ 3x 193B)
+# config for training mymodel (67M) down to very nice loss of ???
+# Ran more 1,725,000 iterations on 1 node with 1x H100 95GB
+# For a total of 420B tokens (~ 3x 140B)
 # launch as the following (e.g. in a screen session) and wait ~5 days:
-# $ torchrun --standalone --nproc_per_node=2 train.py config/train_mymodel.py
+# python train.py config/train_mytokenizer.py
 
 wandb_log = True
 wandb_project = 'mytokenizer'
@@ -19,8 +18,8 @@ block_size = 512
 gradient_accumulation_steps = 4 * 1
 
 # this makes total number of tokens be ~193B
-max_iters = 2400000
-lr_decay_iters = 2400000
+max_iters = 1725000
+lr_decay_iters = 1725000
 learning_rate = 6e-4 # max learning rate
 min_lr = 9e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
 
